5d4f9e91d9852c8bc0b2df2917071f5d0ce31abd
Ian Holmes <iholmes@uline.com>
2021-09-09 17:26:49 -0500

    fixed typo and bug with status not updating properly in loadTasks() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -217,37 +217,6 @@ var loadTasks = function() {
             tasksCompletedEl.appendChild(listItemEl);
         }
     }
-
-    // for (var i = 0; i < tasks.length; i++) {
-    //     taskIdCounter = tasks[i].id;
-
-    //     var listItemEl = document.createElement("li");
-    //     listItemEl.className = "task-item";
-    //     listItemEl.setAttribute("data-task-id", tasks[i].id)
-
-    //     var taskInfoEl = document.createElement("div");
-    //     taskInfoEl.className = "task-info";
-    //     taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-    //     listItemEl.appendChild(taskInfoEl);
-
-    //     var taskActionEl = createTaskActions(tasks[i].id);
-    //     listItemEl.appendChild(taskActionEl);
-
-    //     if (tasks[i].status === "to do") {
-    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-    //         tasksToDoEl.appendChild(listItemEl);
-    //     }
-    //     else if (tasks[i].status === "in progress") {
-    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-    //         tasksInProgressEl.appendChild(listItemEl);
-    //     }
-    //     else if (tasks[i].status === "completed") {
-    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-    //         tasksCompletedEl.appendChild(listItemEl);
-    //     }
-    //     taskIdCounter++;
-    //     console.log(listItemEl);
-    // }
 }
 
 formEl.addEventListener("submit", taskFormHandler);
