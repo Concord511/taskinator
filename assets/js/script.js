8a237f15709dbcf1ac0dcac2766d76a8bcc9b2ca
Ian Holmes <iholmes@uline.com>
2021-09-09 17:06:38 -0500

    added ability to load tasks from previous sessions

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,6 +101,7 @@ var createTaskEl = function(taskDataObj) {
     saveTasks();
 
     taskIdCounter++;
+    return listItemEl;
 }
 
 var createTaskActions = function(taskId) {
@@ -191,6 +192,66 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (tasks === null) {
+        return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+    
+    for (var i = 0; i < savedTasks.length; i++) {
+        var listItemEl = createTaskEl(savedTasks[i]);
+        console.log(listItemEl);
+        if (savedTasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (savedTasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (savedTasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+    }
+
+    // for (var i = 0; i < tasks.length; i++) {
+    //     taskIdCounter = tasks[i].id;
+
+    //     var listItemEl = document.createElement("li");
+    //     listItemEl.className = "task-item";
+    //     listItemEl.setAttribute("data-task-id", tasks[i].id)
+
+    //     var taskInfoEl = document.createElement("div");
+    //     taskInfoEl.className = "task-info";
+    //     taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    //     listItemEl.appendChild(taskInfoEl);
+
+    //     var taskActionEl = createTaskActions(tasks[i].id);
+    //     listItemEl.appendChild(taskActionEl);
+
+    //     if (tasks[i].status === "to do") {
+    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+    //         tasksToDoEl.appendChild(listItemEl);
+    //     }
+    //     else if (tasks[i].status === "in progress") {
+    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+    //         tasksInProgressEl.appendChild(listItemEl);
+    //     }
+    //     else if (tasks[i].status === "completed") {
+    //         listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+    //         tasksCompletedEl.appendChild(listItemEl);
+    //     }
+    //     taskIdCounter++;
+    //     console.log(listItemEl);
+    // }
+}
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
